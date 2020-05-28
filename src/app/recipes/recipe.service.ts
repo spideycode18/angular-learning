6a0d78b7d17cb5525c90a8a18fb16abb0e9043fe
hoang.phan <hoang.phan@sai-digital.com>
2020-05-28 14:10:54 +0700

    complete section 10

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,24 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Recipe', "This is a first Recipe", "https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png"),
-    new Recipe('A other Recipe', "This is a second Recipe", "https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png")
+    new Recipe(
+      'A Recipe',
+      "This is a first Recipe",
+      "https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png",
+      [
+        new Ingredient('Meat', 5),
+        new Ingredient('Chicken', 3)
+      ]
+      ),
+    new Recipe(
+      'A other Recipe',
+      "This is a second Recipe",
+      "https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png",
+      [
+        new Ingredient('Mushroom', 2),
+        new Ingredient('Vegetables', 7)
+      ]
+    )
   ];
   constructor() { }
 
